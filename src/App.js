5fbe27817fcaf85c2579d3ffd1dfe0dfcedce20b
NQT <tnquan.95@gmail.com>
2024-07-18 11:37:49 +1000

    add Main component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './Header';
+import Main from './Main';
 import Nav from './Nav';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header/>
       <Nav/>
+      <Main/>
     </div>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,5 @@
+export default Main = () => {
+    return (
+        <main></main>
+    )
+}
