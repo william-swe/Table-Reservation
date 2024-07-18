ee9e8acd34d2cf553301d6d0dd7df401c1ea48a4
NQT <tnquan.95@gmail.com>
2024-07-18 11:38:20 +1000

    add Footer component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './Footer';
 import Header from './Header';
 import Main from './Main';
 import Nav from './Nav';
@@ -9,6 +10,7 @@ function App() {
       <Header/>
       <Nav/>
       <Main/>
+      <Footer/>
     </div>
   );
 }
